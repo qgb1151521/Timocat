47c417c4a2bfe2d41622bad7bf5e176e3f4cd0e9
Lellansin <lellansin@gmail.com>
2015-01-27 14:24:28 +0800

    多文件测试

---
+++ src/timocat/FileProc.java
@@ -0,0 +1,12 @@
+package timocat;
+
+public class FileProc {
+
+    public static void main(String[] args) {
+        System.out.println("hello file proc ?");
+    }
+
+    public void test() {
+        System.out.println("hello file");
+    }
+}
